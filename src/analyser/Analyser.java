53bec4f7ab9d93e03c5f0b023d866a10f9ab4a32
linan1109 <linan119@yeah.net>
2020-09-23 19:19:12 +0800

    init

---
+++ src/analyser/Analyser.java
@@ -0,0 +1,45 @@
+package analyser;
+
+import error.CompilationError;
+
+import java.util.Optional;
+
+public final class Analyser {
+
+    public Optional<CompilationError> Analyser(){
+        return Optional.empty();
+    }
+
+    // <程序>
+    private Optional<CompilationError> analyseProgram(){
+        return Optional.empty();
+    }
+
+    private Optional<CompilationError> analyseMain(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseConstantDeclaration(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseVariableDeclaration(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseStatementSequence(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseConstantExpression(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseExpression(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseAssignmentStatement(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseItem(){
+        return Optional.empty();
+    }
+    private Optional<CompilationError> analyseFactor(){
+        return Optional.empty();
+    }
+}
---
+++ src/error/ErrorCode.java
@@ -0,0 +1,4 @@
+package error;
+
+public enum ErrorCode {
+}
