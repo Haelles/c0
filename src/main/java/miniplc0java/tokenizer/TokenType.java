c5c36cc0301b1027ae70d01f9c1cb78bf962e4d3
haelles <haelle@buaa.edu.cn>
2020-12-17 00:11:06 +0800

    词法的初始化

--- src/main/java/miniplc0java/tokenizer/TokenType.java
+++ src/main/java/miniplc0java/tokenizer/TokenType.java
@@ -1,78 +1,137 @@
 package miniplc0java.tokenizer;
 
 public enum TokenType {
-    /** 空 */
-    None,
-    /** 无符号整数 */
-    Uint,
-    /** 标识符 */
-    Ident,
-    /** Begin */
-    Begin,
-    /** End */
-    End,
-    /** Var */
-    Var,
-    /** Const */
-    Const,
-    /** Print */
-    Print,
-    /** 加号 */
-    Plus,
-    /** 减号 */
-    Minus,
-    /** 乘号 */
-    Mult,
-    /** 除号 */
-    Div,
-    /** 等号 */
-    Equal,
-    /** 分号 */
-    Semicolon,
-    /** 左括号 */
-    LParen,
-    /** 右括号 */
-    RParen,
-    /** 文件尾 */
+    // 关键字
+    FN_KW,   //  -> 'fn'
+    LET_KW, //   -> 'let'
+    CONST_KW, //  -> 'const'
+    AS_KW, //     -> 'as'
+    WHILE_KW, //  -> 'while'
+    IF_KW, //     -> 'if'
+    ELSE_KW, //   -> 'else'
+    RETURN_KW, // -> 'return'
+
+    // 这两个是扩展 c0 的关键字
+    BREAK_KW, //  -> 'break'
+    CONTINUE_KW, // -> 'continue'
+
+    // 字面量
+    UINT_LITERAL, // -> digit+
+    STRING_LITERAL, // -> '"' (string_regular_char | escape_sequence)* '"'
+    CHAR_LITERAL, // -> '\'' (char_regular_char | escape_sequence) '\''
+    // 扩展 c0
+    DOUBLE_LITERAL, // -> digit+ '.' digit+ ([eE] [+-]? digit+)?
+
+    // 标识符
+    IDENT, // -> [_a-zA-Z] [_a-zA-Z0-9]*
+
+    // 运算符
+    PLUS, //      -> '+'
+    MINUS, //     -> '-'
+    MUL, //       -> '*'
+    DIV, //       -> '/'
+    ASSIGN, //    -> '='
+    EQ, //        -> '=='
+    NEQ, //       -> '!='
+    LT, //        -> '<'
+    GT, //        -> '>'
+    LE, //        -> '<='
+    GE, //        -> '>='
+    L_PAREN, //   -> '('
+    R_PAREN, //   -> ')'
+    L_BRACE, //   -> '{'
+    R_BRACE, //   -> '}'
+    ARROW, //     -> '->'
+    COMMA, //     -> ','
+    COLON, //     -> ':'
+    SEMICOLON, // -> ';'
+
+    // 注释，这是扩展c0内容
+    COMMENT, // -> '//' regex(.*) '\n'
+
+    // 额外添加，文件尾
     EOF;
 
     @Override
     public String toString() {
         switch (this) {
-            case None:
-                return "NullToken";
-            case Begin:
-                return "Begin";
-            case Const:
+            case FN_KW:
+                return "FunctionKeyword";
+            case LET_KW:
+                return "Let";
+            case CONST_KW:
                 return "Const";
-            case Div:
-                return "DivisionSign";
+            case AS_KW:
+                return "As";
+            case WHILE_KW:
+                return "While";
+            case IF_KW:
+                return "If";
+            case ELSE_KW:
+                return "Else";
+            case RETURN_KW:
+                return "Return";
+
+            case BREAK_KW:
+                return "Break";
+            case CONTINUE_KW:
+                return "Continue";
+
+            case UINT_LITERAL:
+                return "UnsignedInteger";
+            case STRING_LITERAL:
+                return "String";
+            case CHAR_LITERAL:
+                return "Char";
+
+            case DOUBLE_LITERAL:
+                return "Double";
+
+            case IDENT:
+                return "Identifier";
+
+            case PLUS:
+                return "PlusSign";
+            case MINUS:
+                return "MinusSign";
+            case MUL:
+                return "MultiplicationSign";
+            case DIV:
+                return "DivideSign";
+            case ASSIGN:
+                return "Assign";
+            case EQ:
+                return "Equal";
+            case NEQ:
+                return "NotEqual";
+            case LT:
+                return "LessThan";
+            case GT:
+                return "GreaterThan";
+            case LE:
+                return "LessOrEqualThan";
+            case GE:
+                return "GreaterOrEqualThan";
+            case L_PAREN:
+                return "LeftBracket";
+            case R_PAREN:
+                return "RightBracket";
+            case L_BRACE:
+                return "LightBrace";
+            case R_BRACE:
+                return "RightBrace";
+            case ARROW:
+                return "RightArrow";
+            case COMMA:
+                return "Comma";
+            case SEMICOLON:
+                return "Semicolon";
+            case COLON:
+                return "Colon";
+            case COMMENT:
+                return "Comment";
             case EOF:
                 return "EOF";
-            case End:
-                return "End";
-            case Equal:
-                return "EqualSign";
-            case Ident:
-                return "Identifier";
-            case LParen:
-                return "LeftBracket";
-            case Minus:
-                return "MinusSign";
-            case Mult:
-                return "MultiplicationSign";
-            case Plus:
-                return "PlusSign";
-            case Print:
-                return "Print";
-            case RParen:
-                return "RightBracket";
-            case Semicolon:
-                return "Semicolon";
-            case Uint:
-                return "UnsignedInteger";
-            case Var:
-                return "Var";
             default:
                 return "InvalidToken";
         }
--- src/main/java/miniplc0java/tokenizer/Tokenizer.java
+++ src/main/java/miniplc0java/tokenizer/Tokenizer.java
@@ -3,15 +3,23 @@ package miniplc0java.tokenizer;
 import miniplc0java.error.TokenizeError;
 import miniplc0java.error.ErrorCode;
 import miniplc0java.util.Pos;
+import org.checkerframework.checker.units.qual.C;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Tokenizer {
 
     private StringIter it;
+    private StringBuilder buffer;
+    private char peek;
+    private char cur;
 
     public Tokenizer(StringIter it) {
         this.it = it;
+        this.buffer = new StringBuilder();
+        this.peek = 0;
+        this.cur = 0;
     }
 
     // 这里本来是想实现 Iterator<Token> 的，但是 Iterator 不允许抛异常，于是就这样了
@@ -31,40 +39,53 @@ public class Tokenizer {
             return new Token(TokenType.EOF, "", it.currentPos(), it.currentPos());
         }
 
-        char peek = it.peekChar();
-        if (Character.isDigit(peek)) {
-            return lexUInt();
-        } else if (Character.isAlphabetic(peek)) {
+        char cur = it.nextChar();
+        if (Character.isDigit(cur)) {
+            return lexUIntOrDouble();
+        } else if (cur == '_' || Character.isLowerCase(cur) || Character.isUpperCase(cur)) {
             return lexIdentOrKeyword();
-        } else {
+        } else{
             return lexOperatorOrUnknown();
         }
     }
 
-    private Token lexUInt() throws TokenizeError {
-        // 请填空：
-        // 直到查看下一个字符不是数字为止:
-        // -- 前进一个字符，并存储这个字符
-        //
-        // 解析存储的字符串为无符号整数
-        // 解析成功则返回无符号整数类型的token，否则返回编译错误
-        //
-        // Token 的 Value 应填写数字的值
+    private Token lexUIntOrDouble() throws TokenizeError {
+        buffer.setLength(0); // 清空缓存
         Pos startPos = it.currentPos();
-        try{
-            StringBuilder temp = new StringBuilder();
-            temp.append(it.nextChar());
-            do{
-                char ch = it.peekChar();
-                if(Character.isDigit(ch)){
-                    temp.append(ch);
-                    it.nextChar();
+        buffer.append(cur);
+        peek = it.peekChar();
+        while(Character.isDigit(peek)){
+            buffer.append(peek);
+            swapCurPeek();
+        }
+        if(peek == '.'){ // 读到小数点
+            buffer.append(peek);
+            swapCurPeek();
+            if(!Character.isDigit(peek)){ // 小数点下一位不是数字，非法（至少要有一位数字）
+                throw new TokenizeError(ErrorCode.InvalidInput, it.currentPos());
+            }
+            while(Character.isDigit(peek)){
+                buffer.append(peek);
+                swapCurPeek();
+            }
+            if(peek == 'e' || peek == 'E'){
+                buffer.append(peek);
+                swapCurPeek();
+                if(peek == '+' || peek == '-'){
+                    buffer.append(peek);
+                    swapCurPeek();
                 }
-                else break;
-            }while (true);
-            return new Token(TokenType.Uint, Integer.parseInt(temp.toString()), startPos, it.currentPos());
-        } catch (Exception e) {
-            throw new TokenizeError(ErrorCode.IntegerOverflow, startPos);
+                if(!Character.isDigit(peek)) // 至少要有一位数字
+                    throw new TokenizeError(ErrorCode.InvalidInput, it.currentPos());
+                while (Character.isDigit(peek)){
+                    buffer.append(peek);
+                    swapCurPeek();
+                }
+            }
+            return new Token(TokenType.DOUBLE_LITERAL, Double.parseDouble(buffer.toString()), startPos, it.currentPos());
+        }
+        else{ // 无符号整数
+            return new Token(TokenType.UINT_LITERAL, Integer.parseInt(buffer.toString()), startPos, it.currentPos());
         }
     }
 
@@ -141,5 +162,10 @@ public class Tokenizer {
         while (!it.isEOF() && Character.isWhitespace(it.peekChar())) {
             it.nextChar();
         }
+    }
+
+    private void swapCurPeek(){
+        cur = it.nextChar();
+        peek = it.peekChar();
     }
 }
